cc6d79d3eb3180349dfa39593468bae2b8f33d5e
GHOST <ghostdevbusiness@gmail.com>
2024-07-25 05:55:49 +0100

    refactor: switch to deno

--- src/commands.ts
+++ src/commands.ts
@@ -1,49 +1,133 @@
-type Engine = [name: string, resolver: string | ((args: string[]) => string)];
+import { Table } from 'jsr:@sauber/table@0.1.0';
+import ansiRegex from 'npm:ansi-regex';
 
-// prettier-ignore
-export const commands: Record<string, Engine> = {
-    // Search Engines
-    ddg: ['DuckDuckGo', 'https://duckduckgo.com/?q=$#'],
-    brave: ['Brave Search', 'https://search.brave.com/search?q=$#'],
+interface Command {
+	name: string;
+	description: string;
+	aliases?: string[];
+	handle: string | ((args: string[]) => string);
+}
 
-    // Dev
-    gh: ['GitHub', ([query]) => {
-            if (/(\w)+\/(\w)+/g.test(query)) {
-                return `https://github.com/${query}`;
-            }
+export const commands: Command[] = [
+	{
+		name: 'ddg',
+		description: 'DuckDuckGo',
+		handle: 'https://duckduckgo.com/?q=$#',
+	},
+	{
+		name: 'brave',
+		description: 'Brave Search',
+		handle: 'https://search.brave.com/search?q=$#',
+	},
+	{
+		name: 'gh',
+		description: 'GitHub',
+		handle: ([query]) => {
+			if (/(\w)+\/(\w)+/g.test(query)) {
+				return `https://github.com/${query}`;
+			}
 
-            return 'https://github.com/search?q=$#';
-        }],
+			return 'https://github.com/search?q=$#';
+		},
+	},
+	{
+		name: 'ghs',
+		description: 'Github Self',
+		handle: ([query]) =>
+			`https://github.com/ghostdevv${query?.length ? `/${query}` : ''}`,
+	},
+	{
+		name: 'npm',
+		description: 'Npm',
+		handle: 'https://npmjs.com/search?q=$#',
+	},
+	{
+		name: 'ts',
+		description: 'TypeScript',
+		handle: 'https://www.typescriptlang.org/play',
+	},
+	{
+		name: 'mdn',
+		description: 'MDN',
+		handle: 'https://developer.mozilla.org/en-US/search?q=$#',
+	},
+	{
+		name: 'crate',
+		aliases: ['crates'],
+		description: 'Crates.io',
+		handle: 'https://crates.io/search?q=$#',
+	},
+	{
+		name: 'pypi',
+		description: 'pypi',
+		handle: 'https://pypi.org/search/?q=$#',
+	},
+	{
+		name: 'gitpod',
+		description: 'Gitpod',
+		handle: 'https://gitpod.io/#https://github.com/$0',
+	},
+	{
+		name: 'whois',
+		description: 'WhoIs',
+		handle: 'https://whois.com/whois/$0',
+	},
+	{
+		name: 'mod',
+		description: 'Modrinth',
+		handle: 'https://modrinth.com/mods?q=$#',
+	},
+	{
+		name: 'mods',
+		description: 'Modrinth',
+		handle: 'https://modrinth.com/mods?q=$#',
+	},
+	{
+		name: 'rtt',
+		description: 'realtimetrains',
+		handle: 'https://www.realtimetrains.co.uk/search/simple/gb-nr:$0',
+	},
+	{
+		name: 'train',
+		description: 'train times',
+		handle: 'https://traintimes.org.uk/$0/$1',
+	},
+	{
+		name: 'yt',
+		description: 'Youtube',
+		handle: 'https://www.youtube.com/results?search_query=$#',
+	},
+	{
+		name: 'aur',
+		description: 'AUR',
+		handle: 'https://aur.archlinux.org/packages?K=$#',
+	},
+	{
+		name: 'arch',
+		description: 'Arch Forums',
+		handle: 'https://bbs.archlinux.org/search.php?action=search&keywords=$#',
+	},
+	{
+		name: 'cf',
+		description: 'Cloudflare Shortcut',
+		handle: 'https://dash.cloudflare.com/44321e1b7fbd29b56b65c1890a3bbee2',
+	},
+	{ name: 'ip', description: 'IP', handle: 'https://ip.willow.sh' },
+	{
+		name: 'help',
+		aliases: ['?'],
+		description: 'List all the commands',
+		handle() {
+			const table = new Table();
+			table.theme = Table.wideTheme;
+			table.headers = ['Command', 'Aliases', 'Description'];
+			table.rows = commands.map((command) => [
+				command.name,
+				command.aliases?.join(', ') || '',
+				command.description,
+			]);
 
-    ghs: ['Github Self', ([query]) => {
-        return `https://github.com/ghostdevv${query?.length ? `/${query}` : ''}`
-    }],
-        
-    npm: ['Npm', 'https://npmjs.com/search?q=$#'],
-    ts: ['TypeScript', 'https://www.typescriptlang.org/play'],
-    mdn: ['MDN', 'https://developer.mozilla.org/en-US/search?q=$#'],
-    crates: ['Crates.io', 'https://crates.io/search?q=$#'],
-    crate: ['Crates.io', 'https://crates.io/search?q=$#'],
-    pypi: ['pypi', 'https://pypi.org/search/?q=$#'],
-    gitpod: ['Gitpod', ([q]) => `https://gitpod.io/#https://github.com/${q}`],
-    whois: ['WhoIs', 'https://whois.com/whois/$0'],
-    mod: ['Modrinth', 'https://modrinth.com/mods?q=$#'],
-    mods: ['Modrinth', 'https://modrinth.com/mods?q=$#'],
-
-    // Trains
-    rtt: ['realtimetrains', 'https://www.realtimetrains.co.uk/search/simple/gb-nr:$0'],
-    train: ['train times', 'https://traintimes.org.uk/$0/$1'],
-
-    // Video
-    yt: ['Youtube', 'https://www.youtube.com/results?search_query=$#'],
-
-    // Linux
-    aur: ['AUR', 'https://aur.archlinux.org/packages?K=$#'],
-    arch: ['Arch Forums', 'https://bbs.archlinux.org/search.php?action=search&keywords=$#'],
-
-    // Links
-    cf: ['Cloudflare Shortcut', 'https://dash.cloudflare.com/44321e1b7fbd29b56b65c1890a3bbee2'],
-
-    // Misc
-    ip: ['IP', 'https://ip.willow.sh'],
-};
+			return table.toString().replace(ansiRegex(), '');
+		},
+	},
+];
--- src/types.ts
+++
@@ -1,6 +0,0 @@
-import type { Env as HonoEnv } from 'hono';
-
-//? Hono env config
-export interface Env extends HonoEnv {
-	Bindings: {};
-}
