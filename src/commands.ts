1274e8b949c7399063093426d4a1bc313c114fc3
GHOST <ghostdevbusiness@gmail.com>
2024-07-25 04:39:10 +0100

    chore: removce unused

--- src/commands.ts
+++ src/commands.ts
@@ -4,7 +4,6 @@ type Engine = [name: string, resolver: string | ((args: string[]) => string)];
 export const commands: Record<string, Engine> = {
     // Search Engines
     ddg: ['DuckDuckGo', 'https://duckduckgo.com/?q=$#'],
-    yandex: ['Yandex', 'https://yandex.com/search/?text=$#'],
 
     // Dev
     gh: ['GitHub', ([query]) => {
@@ -16,27 +15,12 @@ export const commands: Record<string, Engine> = {
         }],
 
     ghs: ['Github Self', 'https://github.com/ghostdevv/$0'],
-
-    svelte: ['Svelte', ([query]) => {
-            switch (query) {
-                case 'docs':
-                    return 'https://svelte.dev/docs';
-
-                default:
-                case 'repl':
-                    return 'https://svelte.dev/repl';
-
-                case 'kit':
-                    return 'https://kit.svelte.dev';
-            }
-        }],
         
     npm: ['Npm', 'https://npmjs.com/search?q=$#'],
     ts: ['TypeScript', 'https://www.typescriptlang.org/play'],
     mdn: ['MDN', 'https://developer.mozilla.org/en-US/search?q=$#'],
     crates: ['Crates.io', 'https://crates.io/search?q=$#'],
     pypi: ['pypi', 'https://pypi.org/search/?q=$#'],
-    djs: ['discord.js', 'https://discord.js.org/#/docs/discord.js/stable/search?query=$#'],
     gitpod: ['Gitpod', ([q]) => `https://gitpod.io/#https://github.com/${q}`],
     whois: ['WhoIs', 'https://whois.com/whois/$0'],
     mod: ['Modrinth', 'https://modrinth.com/mods?q=$#'],
@@ -44,9 +28,6 @@ export const commands: Record<string, Engine> = {
 
     // Video
     yt: ['Youtube', 'https://www.youtube.com/results?search_query=$#'],
-
-    // Images
-    pexels: ['Pexels', 'https://www.pexels.com/search/$#'],
 
     // Linux
     aur: ['AUR', 'https://aur.archlinux.org/packages?K=$#'],
@@ -56,8 +37,5 @@ export const commands: Record<string, Engine> = {
     cf: ['Cloudflare Shortcut', 'https://dash.cloudflare.com/44321e1b7fbd29b56b65c1890a3bbee2'],
 
     // Misc
-    l: ['LocalHost', ([port, ...q]) => `http://localhost:${port}/${q.map(encodeURIComponent).join('/')}`],
     ip: ['IP', 'https://ip.willow.sh'],
-    echo: ['Echo', 'https://echo.willow.sh?q=$#'],
-    secret: ['Echo Encode (secret)', ([...q]) => `https://echo.willow.sh/?encoded=1&q=${encodeURIComponent(btoa(q.join(' ')))}`],
 };
