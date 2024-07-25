9f4203eb4d54269adf91c94cc5ee16d29138935d
GHOST <ghostdevbusiness@gmail.com>
2024-07-25 04:44:32 +0100

    feat: add new commands

--- src/commands.ts
+++ src/commands.ts
@@ -4,6 +4,7 @@ type Engine = [name: string, resolver: string | ((args: string[]) => string)];
 export const commands: Record<string, Engine> = {
     // Search Engines
     ddg: ['DuckDuckGo', 'https://duckduckgo.com/?q=$#'],
+    brave: ['Brave Search', 'https://search.brave.com/search?q=$#'],
 
     // Dev
     gh: ['GitHub', ([query]) => {
@@ -14,17 +15,24 @@ export const commands: Record<string, Engine> = {
             return 'https://github.com/search?q=$#';
         }],
 
-    ghs: ['Github Self', 'https://github.com/ghostdevv/$0'],
+    ghs: ['Github Self', ([query]) => {
+        return `https://github.com/ghostdevv${query?.length ? `/${query}` : ''}`
+    }],
         
     npm: ['Npm', 'https://npmjs.com/search?q=$#'],
     ts: ['TypeScript', 'https://www.typescriptlang.org/play'],
     mdn: ['MDN', 'https://developer.mozilla.org/en-US/search?q=$#'],
     crates: ['Crates.io', 'https://crates.io/search?q=$#'],
+    crate: ['Crates.io', 'https://crates.io/search?q=$#'],
     pypi: ['pypi', 'https://pypi.org/search/?q=$#'],
     gitpod: ['Gitpod', ([q]) => `https://gitpod.io/#https://github.com/${q}`],
     whois: ['WhoIs', 'https://whois.com/whois/$0'],
     mod: ['Modrinth', 'https://modrinth.com/mods?q=$#'],
     mods: ['Modrinth', 'https://modrinth.com/mods?q=$#'],
+
+    // Trains
+    rtt: ['realtimetrains', 'https://www.realtimetrains.co.uk/search/simple/gb-nr:$0'],
+    train: ['train times', 'https://traintimes.org.uk/$0/$1'],
 
     // Video
     yt: ['Youtube', 'https://www.youtube.com/results?search_query=$#'],
