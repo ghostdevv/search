da9fb05ddac5761a601e11ae681c1d49be971d77
GHOST <ghostdevbusiness@gmail.com>
2024-07-26 03:20:53 +0100

    feat: add cli

--- src/cli.ts
+++ src/cli.ts
@@ -1,7 +1,38 @@
+import pkg from '../deno.json' with { type: 'json' };
 import { parseArgs } from '@std/cli';
+import dedent from 'dedent';
+
+interface CommandHelpOptions {
+	name: string;
+	description: string;
+	flags: { flag: string; description: string }[];
+}
+
+export function print_cmd_help(options: CommandHelpOptions): never {
+	options.flags.push({ flag: '-h, --help', description: 'Display this message' });
+
+	console.log(dedent`
+		Description
+		  ${options.description}
+
+		Usage
+		  $ search ${options.name} [options]
+
+		Options
+		  ${options.flags.map((f) => `${f.flag}\t\t${f.description}`).join('\n')}
+	`);
+
+	Deno.exit(1);
+}
 
 export function parse_cli<T extends readonly string[]>(valid_commands: T) {
 	const args = parseArgs(Deno.args);
+
+	if (args.v || args.version) {
+		console.log(pkg.version);
+		Deno.exit(0);
+	}
+
 	const [command] = args._;
 
 	const is_valid_command = valid_commands.includes(`${command}`);
@@ -12,7 +43,19 @@ export function parse_cli<T extends readonly string[]>(valid_commands: T) {
 		command == 'help' ||
 		!is_valid_command
 	) {
-		console.log('search help page tbd');
+		console.log(dedent`Search v${pkg.version}
+
+			Usage:
+			  $ search <command> [options]
+
+			Available Commands:
+			  serve		Serve the search server for use with browsers
+
+			Options
+			  -v, --version		Display the current version
+			  -h, --help		Display this message
+		`);
+
 		Deno.exit(0);
 	}
 
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { parse_cli } from './cli.ts';
+import { parse_cli, print_cmd_help } from './cli.ts';
 import { serve } from './server.ts';
 
 const VALID_COMMANDS = ['serve'] as const;
@@ -7,6 +7,14 @@ const { command, help, args } = parse_cli(VALID_COMMANDS);
 
 switch (command) {
 	case 'serve': {
+		if (help) {
+			print_cmd_help({
+				name: 'serve',
+				description: 'Serve the search server for use with browsers',
+				flags: [],
+			});
+		}
+
 		const port_arg = args.port && parseInt(args.port);
 		const port = isNaN(port_arg) ? 9343 : port_arg;
 		serve(port);
